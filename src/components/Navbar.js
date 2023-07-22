f68de14004eb8e6819f953e1bb475c9cd13492b6
Goutam <vachas369@gmail.com>
2023-07-22 16:52:13 +0530

    Github repo link for portfolio updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/VGOUTAM/Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
